45951b26af8c8740931e32864d4df95f653b83a5
Tima Tolochko <timur10645@icloud.com>
2016-10-23 14:13:11 +0300

    without xml and web.xml

---
+++ spring3annotationHelloWorld/src/main/java/com/github/servlet3/MyWebInitializer.java
@@ -0,0 +1,25 @@
+package com.github.servlet3;
+
+import com.github.config.SpringWebConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+
+public class MyWebInitializer extends
+        AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] { SpringWebConfig.class };
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
+    }
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+}
